b3ff2b3437da644868b91e55dfcd8fc3fa23d202
felixfaisal <faisalahmedfarooq46@gmail.com>
2019-06-19 15:11:48 +0530

    Final modifications

---
+++ infogather.py
@@ -0,0 +1,39 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import datetime
+currentDT = datetime.datetime.now()
+time = str(currentDT)
+my_url ='https://www.whois.com/whois/'
+url =input("Enter domain name :")
+newurl = my_url+url
+my_url = newurl
+uClient= uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+page_soup = soup(page_html,'html.parser')
+con=page_soup.findAll("div",{"class":"whois_main_column"})
+co=con[0].findAll("div",{"class":"df-block"})
+filename="domain.csv"
+f = open(filename, "a")
+print("Information as on " + time + "\n")
+f.write("Obtained information at " + time + "\n")
+for i in range(0,len(co)):
+	#print(containers[i].text+" "+container[i].text)
+	header = co[i].findAll("div",{"class":"df-heading"})
+	f.write(header[0].text.upper()+"\n")
+	print(header[0].text.upper())	
+	label  = co[i].findAll("div",{"class":"df-label"})
+	values = co[i].findAll("div",{"class":"df-value"})
+	for j in range(0,len(label)):
+		print(label[j].text+" "+values[j].text)
+		f.write(label[j].text.replace(":","")+","+values[j].text.replace(",","")+"\n")
+	
+	print("\n")
+	f.write("\n")
+
+f.write("\n")	
+f.close()
+			
+
+	
+
